8b80ea4f44b7c4d1a4d8b89e10c3bf33625909f2
Feranen <127853064+Feranen@users.noreply.github.com>
2026-01-23 15:58:26 +0100

    zadanie 23.01.2026

---
+++ Database/WeatherStationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Database
+{
+    public class WeatherStationContext : DbContext
+    {
+      
+      DbSet<User>  Users { get; set; }
+      DbSet<Sensor> Sensor { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate = False; Application Intent = ReadWrite; Multi Subnet Failover = False");
+        }
+    }
+
+}
+
--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -1,11 +1,15 @@
 ﻿using Database;
 
-User user1 = new User("rikovens", 999);
-User user2 = new User("Danill", 999);
-Sensor sensor1 = new Sensor();
 
-Console.WriteLine($"Utworzono użytkownika 1: {user1.Name} (ID: {user1.Id})");
-Console.WriteLine($"Utworzono użytkownika 2: {user2.Name} (ID: {user2.Id})");
-Console.WriteLine("Utworzono sensor 1");
-bool i = user1.Equals( user2 );
-Console.WriteLine(i);
+namespace WetherStation;
+
+
+public class Program
+
+{
+    public static void Main(string[] args);
+}
+    
+    
+
+   
